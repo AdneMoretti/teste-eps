23a80b43eeaeba4558239ea799ad9292ed1ed24e
morettipdr <hawkfx123@gmail.com>
2024-04-04 09:47:21 -0300

    solved problems on starting server and webapp

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,6 @@
 import app from './app'
 
-app.listen(3000, () => console.log('Server is running here'));
+app.listen(3333, () => console.log('Server is running here: https:/localhost:3000'));
+app.get("/", (req, res) => {
+    res.send("status: active")
+})
